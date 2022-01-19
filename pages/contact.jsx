987627443ec9c9cad135b71c81f806620059df0b
bskcontactwise <bsatheeshkumar@gmail.com>
2022-01-19 17:00:07 +0530

    form button changed

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -195,23 +195,9 @@ function Contact() {
                     margin="normal"
                   />
                   <hr />
-                  <p>
-                    <label>
-                      Your Role:{" "}
-                      <select name="role[]" multiple>
-                        <option value="leader">Leader</option>
-                        <option value="follower">Follower</option>
-                      </select>
-                    </label>
-                  </p>
-                  <p>
-                    <label>
-                      Message: <textarea name="message"></textarea>
-                    </label>
-                  </p>
-                  <p>
-                    <button type="submit">Send</button>
-                  </p>
+                  <Button variant="contained" type="submit" id="SendBtn" fullWidth>
+                    Submit
+                  </Button>
                 </form>
               </Box>
             </Grid>
