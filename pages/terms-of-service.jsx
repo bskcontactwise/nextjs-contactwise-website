7fc9cf294406ae9171b6d468d6ace05c73f6c809
bskcontactwise <bsatheeshkumar@gmail.com>
2022-01-21 18:24:12 +0530

    lint error fixed

--- pages/terms-of-service.jsx
+++ pages/terms-of-service.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Typography from "@mui/material/Typography";
+import p from "@mui/material/p";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import ListItem from "@mui/material/ListItem";
@@ -13,22 +13,22 @@ function Terms() {
     
       <Container>
         <h1 className="my-5">Terms of Service</h1>
-        <Typography>
+        <p>
           These terms of service outline the rules and regulations for the use of ContactWise's Websit
           <br />
-        </Typography>
-        <Typography>
+        </p>
+        <p>
           ContactWise is located at:
           <br />
           Unit 2, Tower House Hoddesdon EN11 8UR - Hertfordshire , United Kingdom
           <br />
-        </Typography>
-        <Typography>
+        </p>
+        <p>
           {" "}
           By accessing this website we assume you accept these terms of service in full. Do not continue to use
           ContactWise's website if you do not accept all of the terms of service stated on this page.
-        </Typography>
-        <Typography>
+        </p>
+        <p>
           The following terminology applies to these Terms of Service, Privacy Statement and Disclaimer Notice and any
           or all Agreements: "Client", "You" and "Your" refers to you, the person accessing this website and accepting
           the Company's terms of service. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company.
@@ -40,68 +40,68 @@ function Terms() {
           use of the above terminology or other words in the singular, plural, capitalisation and/or he/she or they, are
           taken as interchangeable and therefore as referring to same.
           <br />
-        </Typography>
+        </p>
 
         <h5>Cookies</h5>
-        <Typography>
+        <p>
           We employ the use of cookies. By using ContactWise's website you consent to the use of cookies in accordance
           with ContactWise's privacy policy.Most of the modern day interactive web sites use cookies to enable us to
           retrieve user details for each visit. Cookies are used in some areas of our site to enable the functionality
           of this area and ease of use for those people visiting. Some of our affiliate / advertising partners may also
           use cookies.
-        </Typography>
+        </p>
         <h5>License</h5>
-        <Typography>
+        <p>
           {" "}
           Unless otherwise stated, ContactWise and/or it's licensors own the intellectual property rights for all
           material on ContactWise. All intellectual property rights are reserved. You may view and/or print pages from
           https://www.contactwise.io for your own personal use subject to restrictions set in these terms of service.
           <br />
           You must not:
-        </Typography>
-        <Typography>Republish material from https://www.contactwise.io</Typography>
-        <Typography>Sell, rent or sub-license material from https://www.contactwise.io</Typography>
-        <Typography>Reproduce, duplicate or copy material from https://www.contactwise.io</Typography>
-        <Typography>Redistribute content from ContactWise (unless content is specifically made for redistribution).</Typography>
+        </p>
+        <p>Republish material from https://www.contactwise.io</p>
+        <p>Sell, rent or sub-license material from https://www.contactwise.io</p>
+        <p>Reproduce, duplicate or copy material from https://www.contactwise.io</p>
+        <p>Redistribute content from ContactWise (unless content is specifically made for redistribution).</p>
         <h5>Hyperlinking to our Content</h5>
-        <Typography>The following organizations may link to our Web site without prior written approval:</Typography>
-        <Typography>Government agencies;</Typography>
-        <Typography>Search engines;</Typography>
-        <Typography>News organizations;</Typography>
-        <Typography>
+        <p>The following organizations may link to our Web site without prior written approval:</p>
+        <p>Government agencies;</p>
+        <p>Search engines;</p>
+        <p>News organizations;</p>
+        <p>
           Online directory distributors when they list us in the directory may link to our Web site in the same manner
           as they hyperlink to the Web sites of other listed businesses;
-        </Typography>
-        <Typography>
+        </p>
+        <p>
           Systemwide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and
           charity fundraising groups which may not hyperlink to our Web site.
-        </Typography>
+        </p>
         <br />
         <br />
-        <Typography>
+        <p>
           These organizations may link to our home page, to publications or to other Web site information so long as the
           link: (a) is not in any way misleading; (b) does not falsely imply sponsorship, endorsement or approval of the
           linking party and its products or services; and (c) fits within the context of the linking party's site.
-        </Typography>
-        <Typography>
+        </p>
+        <p>
           We may consider and approve in our sole discretion other link requests from the following types of
           organizations:
-        </Typography>
-        <Typography>
+        </p>
+        <p>
           commonly-known consumer and/or business information sources such as Chambers of Commerce, American Automobile
           Association, AARP and Consumers Union;
-        </Typography>
-        <Typography>dot.com community sites;</Typography>
-        <Typography>
+        </p>
+        <p>dot.com community sites;</p>
+        <p>
           associations or other groups representing charities, including charity giving sites, online directory
           distributors;
-        </Typography>
-        <Typography>internet portals;</Typography>
-        <Typography>
+        </p>
+        <p>internet portals;</p>
+        <p>
           accounting, law and consulting firms whose primary clients are businesses; and educational institutions and
           trade associations.
-        </Typography>
-        <Typography>
+        </p>
+        <p>
           We will approve link requests from these organizations if we determine that: (a) the link would not reflect
           unfavorably on us or our accredited businesses (for example, trade associations or other organizations
           representing inherently suspect types of business, such as work-at-home opportunities, shall not be allowed to
@@ -120,50 +120,50 @@ function Terms() {
           or By use of the uniform resource locator (Web address) being linked to; or By use of any other description of
           our Web site or material being linked to that makes sense within the context and format of content on the
           linking party's site.
-        </Typography>
-        <Typography>
+        </p>
+        <p>
           No use of ContactWise's logo or other artwork will be allowed for linking absent a trademark license
           agreement.
-        </Typography>
+        </p>
         <h5>Iframes</h5>
-        <Typography>
+        <p>
           Without prior approval and express written permission, you may not create frames around our Web pages or use
           other techniques that alter in any way the visual presentation or appearance of our Web site.
-        </Typography>
+        </p>
         <h5>Reservation of Rights</h5>
-        <Typography>
+        <p>
           We reserve the right at any time and in its sole discretion to request that you remove all links or any
           particular link to our Web site. You agree to immediately remove all links to our Web site upon such request.
           We also reserve the right to amend these terms of service and its linking policy at any time. By continuing to
           link to our Web site, you agree to be bound to and abide by these linking terms of service.
-        </Typography>
+        </p>
         <h5>Removal of links from our website</h5>
-        <Typography>
+        <p>
           If you find any link on our Web site or any linked web site objectionable for any reason, you may contact us
           about this. We will consider requests to remove links but will have no obligation to do so or to respond
           directly to you.
-        </Typography>
-        <Typography>
+        </p>
+        <p>
           Whilst we endeavour to ensure that the information on this website is correct, we do not warrant its
           completeness or accuracy; nor do we commit to ensuring that the website remains available or that the material
           on the website is kept up to date.
-        </Typography>
+        </p>
         <h5>Content Liability</h5>
-        <Typography>
+        <p>
           We shall have no responsibility or liability for any content appearing on your Web site. You agree to
           indemnify and defend us against all claims arising out of or based upon your Website. No link(s) may appear on
           any page on your Web site or within any context containing content or materials that may be interpreted as
           libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other
           violation of, any third party rights. ‍
-        </Typography>
+        </p>
         <h5>Disclaimer</h5>
-        <Typography>
+        <p>
           To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions
           relating to our website and the use of this website (including, without limitation, any warranties implied by
           law in respect of satisfactory quality, fitness for purpose and/or the use of reasonable care and skill).
           Nothing in this disclaimer will:
-        </Typography>
-        <Typography>
+        </p>
+        <p>
           limit or exclude our or your liability for death or personal injury resulting from negligence;
           <br />
           limit or exclude our or your liability for fraud or fraudulent misrepresentation;
@@ -171,22 +171,22 @@ function Terms() {
           limit any of our or your liabilities in any way that is not permitted under applicable law; or
           <br />
           exclude any of our or your liabilities that may not be excluded under applicable law.
-        </Typography>
-        <Typography>
+        </p>
+        <p>
           The limitations and exclusions of liability set out in this Section and elsewhere in this disclaimer: (a) are
           subject to the preceding paragraph; and (b) govern all liabilities arising under the disclaimer or in relation
           to the subject matter of this disclaimer, including liabilities arising in contract, in tort (including
           negligence) and for breach of statutory duty.
-        </Typography>
-        <Typography>
+        </p>
+        <p>
           To the extent that the website and the information and services on the website are provided free of charge, we
           will not be liable for any loss or damage of any nature.
-        </Typography>
+        </p>
         <h5>Credit & Contact Information</h5>
-        <Typography>
+        <p>
           This Terms of service page was created at privacytermsgenerator.com. If you have any queries regarding any of
           our terms, please contact us.Welcome to ContactWise
-        </Typography>
+        </p>
       </Container>
     
   );
